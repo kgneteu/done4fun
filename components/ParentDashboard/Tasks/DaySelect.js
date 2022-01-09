55798a303bf572e875025ba0d3bf3cf19b6437c1
kgneteu <77528950+kgneteu@users.noreply.github.com>
2022-01-09 22:50:27 +0100

    DatePicker

--- components/ParentDashboard/Tasks/DaySelect.js
+++ components/ParentDashboard/Tasks/DaySelect.js
@@ -1,5 +1,7 @@
 import {useRouter} from "next/router";
-import {FormGroup, ToggleButton} from "@mui/material";
+import {FormControl, FormGroup, FormLabel, ToggleButton} from "@mui/material";
+import {useTranslation} from "next-i18next";
+import Box from "@mui/material/Box";
 
 const getWeekDays = locale => {
     const baseDate = new Date(Date.UTC(2017, 0, 2)); // just a Monday
@@ -13,27 +15,39 @@ const getWeekDays = locale => {
 
 export function DaySelect({mode = 0, name, value, onChange}) {
     const router = useRouter();
-
-    const updateValue = item => {
+    const {t} = useTranslation()
+    const updateValue = (ev, item) => {
         const newValue = value ^ (2 ** item);
-        if (onChange) onChange(newValue)
+        ev.target.value = newValue;
+        ev.target.name = name;
+        if (onChange) onChange(ev)
     };
 
     return (
-        <FormGroup row>
-            {[...Array(mode == 0 ? 7 : 31).keys()].map(item => (
-                <ToggleButton
-                    key={item + 1}
-                    value={item + 1}
-                    selected={value & (2 ** item)}
-                    onChange={() => {
-                        updateValue(item);
-                    }}
-                    sx={{width: "3rem"}}
-                >
-                    {mode == 0 ? getWeekDays(router.locale)[item] : item + 1}
-                </ToggleButton>
-            ))}
-        </FormGroup>
+        <FormControl sx={{
+            borderColor: (theme)=>theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.23)' : 'rgba(255, 255, 255, 0.23)',
+            borderRadius: "4px",
+            borderWidth: 1,
+            p: 1
+        }}>
+            <FormControl component="fieldset"
+
+            >
+                <FormLabel component="legend">{t("Select days")}:</FormLabel>
+                <FormGroup row name={name} variant={"outlined"}>
+                    {[...Array(mode == 0 ? 7 : 31).keys()].map(item => (
+                        <ToggleButton color={"primary"}
+                            key={item + 1}
+                            value={item + 1}
+                            selected={value & (2 ** item)}
+                            onChange={(e) => updateValue(e, item)}
+                            sx={{width: "3rem"}}
+                        >
+                            {mode == 0 ? getWeekDays(router.locale)[item] : item + 1}
+                        </ToggleButton>
+                    ))}
+                </FormGroup>
+            </FormControl>
+         </FormControl>
     )
 }
--- components/ParentDashboard/Tasks/TaskEditor.js
+++ components/ParentDashboard/Tasks/TaskEditor.js
@@ -7,7 +7,9 @@ import {IconPicker} from "../../UI/IconPicker";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import {DaySelect} from "./DaySelect";
-
+import AdapterDateFns from '@mui/lab/AdapterMoment';
+import {DateTimePicker, LocalizationProvider} from "@mui/lab";
+import * as React from "react";
 
 export const TaskEditor = ({task, onClose, onSubmit}) => {
     const {t} = useTranslation();
@@ -15,11 +17,14 @@ export const TaskEditor = ({task, onClose, onSubmit}) => {
         initialValues: {
             action: task ? task.action : '',
             icon: task ? task.icon : '1',
+            points: task ? task.points : '1',
             cycle_type: task ? task.cycle_type : 0,
             selected_days: task ? task.selected_days : 0,
+            start_at: task ? task.start_at : 0,
         },
         validationSchema: yup.object({
             action: yup.string().required(t("Task Name is a required field")),
+            points: yup.number().min(1, t("The allowed range of points is 1-1000")).max(1000, t("The allowed range of points is 1-1000")).required(t("Points is a required field")),
             icon: yup.number(),
             cycle_type: yup.number().min(0).max(4).required(),
         }),
@@ -32,13 +37,13 @@ export const TaskEditor = ({task, onClose, onSubmit}) => {
         formik.setSubmitting(false)
     }
 
-    function monthChanged(value) {
-        formik.setFieldValue("selected_days", value)
-    }
+    // function monthChanged(value) {
+    //     formik.setFieldValue("selected_days", value)
+    // }
 
     function cycleTypeChange(ev) {
         formik.setFieldValue("selected_days", 0).then(
-            ()=>formik.setFieldValue("cycle_type", ev.target.value)
+            () => formik.setFieldValue("cycle_type", ev.target.value)
         )
 
     }
@@ -47,10 +52,28 @@ export const TaskEditor = ({task, onClose, onSubmit}) => {
         <Container maxWidth={"sm"}>
             <form onSubmit={formik.handleSubmit} noValidate>
                 <TextField
-                    {...formFieldProps(formik, "action", t("Action Name"))}
+                    {...formFieldProps(formik, "action", t("Action"))}
                     required
                     autoComplete="task-action"
                     autoFocus
+                />
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DateTimePicker
+                        renderInput={(props) =>
+                            <TextField {...props} />}
+                        label="Start Date"
+                        // value={value}
+                        // onChange={(newValue) => {
+                        //     setValue(newValue);
+                        // }}
+                    />
+                </LocalizationProvider>
+                <TextField
+                    {...formFieldProps(formik, "points", t("Points"))}
+                    required
+                    type={"number"}
+                    pattern="[0-9]{10}"
+                    autoComplete="prize-points"
                 />
                 <IconPicker group={"tasks"}
                             {...formFieldProps(formik, "icon", t("Icon"))}
@@ -69,10 +92,11 @@ export const TaskEditor = ({task, onClose, onSubmit}) => {
 
                 {formik.values["cycle_type"] > 1 &&
                 <DaySelect
+                    id="selected_days"
                     mode={formik.values["cycle_type"] - 2}
-                    name={"selected_days"}
+                    name="selected_days"
                     value={formik.values["selected_days"]}
-                    onChange={event => monthChanged(event)}
+                    onChange={formik.handleChange}
                 />}
 
                 <DialogActions>
